2865389f4ef4d5e211b277d0a378713b0156cb61
Marcio Web <marcio.ascoelho@gmail.com>
2026-04-19 09:17:13 -0300

    adicionando o script 001

--- scripts/script001.py
+++ scripts/script001.py
@@ -1,1 +1,17 @@
+'''
+UFRJ
+Curso : Engenharia Matemática
+Disciplina : Computação 01
+Professor : Henrique Tavares
+Atividade : Avaliação P01
+
+Graduando : Marcio AS Coelho DRE : 126049099
+
+Script:
+    1. Criar um programa que imprime Hello World
+'''
+
+# Este script imprime uma mensagem de 'Hello World' na tela
+
+
 print("Hello World")
