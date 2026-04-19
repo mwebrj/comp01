476e51fe915b165ac7e0ba99f72d9aab3c692216
Marcio Web <marcio.ascoelho@gmail.com>
2026-04-19 09:22:27 -0300

    Adicionando o script 002

---
+++ scripts/script002.py
@@ -0,0 +1,17 @@
+'''
+UFRJ
+Curso : Engenharia Matemática
+Disciplina : Computação 01
+Professor : Henrique Tavares
+Atividade : Avaliação P01
+
+Graduando : Marcio AS Coelho DRE : 126049099
+
+Script:
+    2. Criar um programa que imprime o seu nome 10 vezes
+'''
+
+nome = "Marcio AS Coelho"
+
+for i in range(10):
+    print(nome)
