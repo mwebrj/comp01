e677388575cc4e8e1b94e0f68f16791c8fea84c7
Marcio ASC <marcio.ascoelho@gmail.com>
2026-04-19 18:36:59 -0300

    Adicionado o script 006

---
+++ scripts/script006.py
@@ -0,0 +1,27 @@
+"""
+UFRJ - Universidade Federal do Rio de Janeiro
+
+Curso : Engenharia Matemática
+Disciplina : Computação 01
+Professor : Henrique Tavares
+Atividade : Avaliação P01
+
+- Resolver estes 100 scripts em Python utilizando o github
+
+Graduando : Marcio AS Coelho
+DRE : 126049099
+Github : @mwebrj
+
+Script :
+- 6. Criar um programa para ler dois números e imprimir o maior
+
+"""
+
+num1 = int(input("Informe um número: "))
+maior = num1
+num2 = int(input("Informe o segundo número: "))
+
+if num2 > maior:
+    maior = num2
+
+print(maior)
