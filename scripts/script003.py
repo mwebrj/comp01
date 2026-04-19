240bdfd72351e524f4048036a68224f3ba21991d
Marcio Web <marcio.ascoelho@gmail.com>
2026-04-19 16:58:03 -0300

    Adicionando o script 003

---
+++ scripts/script003.py
@@ -0,0 +1,23 @@
+"""
+UFRJ - Universidade Federal do Rio de Janeiro
+
+Curso : Engenharia Matemática
+Disciplina : Computação 01
+Professor : Henrique Tavares
+Atividade : Avaliação P01
+
+- Resolver estes 100 scripts em Python utilizando o github
+
+Graduando : Marcio AS Coelho
+DRE : 126049099
+Github : @mwebrj
+
+Script :
+- 3. Criar um programa que recebe uma String do usuário e imprime 10 vezes
+
+"""
+
+mensagem = input("Informe a mensagem: ")
+
+for i in range(10):
+    print(mensagem)
