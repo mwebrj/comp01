1c147bd0cbfa2c945fc8999ecacea6c69c47088e
Marcio ASC <marcio.ascoelho@gmail.com>
2026-04-19 18:26:44 -0300

    Adicionado o script 005

---
+++ scripts/script005.py
@@ -0,0 +1,22 @@
+"""
+UFRJ - Universidade Federal do Rio de Janeiro
+
+Curso : Engenharia Matemática
+Disciplina : Computação 01
+Professor : Henrique Tavares
+Atividade : Avaliação P01
+
+- Resolver estes 100 scripts em Python utilizando o github
+
+Graduando : Marcio AS Coelho
+DRE : 126049099
+Github : @mwebrj
+
+Script :
+- 5. Criar um programa para ler um número e imprimir o seu quadrado
+
+"""
+# Script para informar um número  apresentar o quadrado deste número
+num = int(input("Informe o número: "))
+
+print(f"Número: {num:3} - Quadrado: {num ** 2}")
